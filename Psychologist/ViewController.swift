4daf8d282d7c8ddd06b4ff6288345bb853a1a810
Michael Klopf <klopf.michael@gmail.com>
2015-07-17 06:21:27 +0200

    Initial Commit

---
+++ Psychologist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Michael on 17.07.15.
+//  Copyright © 2015 Michael Klopf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
